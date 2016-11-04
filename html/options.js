a8241395ae2d21ad547f14599cee08b9f25235e0
Siddartha Peteti <SiddarthaPeteti@fico.com>
2016-11-04 11:03:14 +0530

    changed event listener

--- html/options.js
+++ html/options.js
@@ -5,9 +5,6 @@ var saveOptions = function () {
     var timeout = document.getElementById('timeout').value;
     storeValue(+timeout);
 };
-var storeGetData = {
-    "storage_get": "timeout"
-};
 chrome.storage.local.get('timeout',(data)=>{
     document.getElementById('timeout').value = data['timeout'];
 })
